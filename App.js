5c8c892490d9351f0fb259e7de7b13b8ab44658a
samme1210 <samme1210@gmail.com>
2023-04-02 22:06:03 +0100

    landing

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 
+import Landing from './components/Landing.jsx';
 import Nav from './components/Nav.jsx'
 
 function App() {
   return (
     <div className='App'>
       <Nav />
+      <Landing />
     </div>
   );
 }
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -26,8 +26,31 @@ function Nav() {
                     <a href="/cart" className="nav__link">
                         <FontAwesomeIcon icon="shopping-cart" />
                     </a>
+                    <span className="cart__length">2</span>
                 </li>
             </ul>
+            <div className="menu__backdrop">
+                <button className='btn__menu btn__menu--close'>
+                    <FontAwesomeIcon icon="times" />
+                </button>
+                <ul className="menu__links">
+                    <li className="menu__list">
+                        <a href="/" className='menu__link'>
+                            Home
+                        </a>
+                    </li>
+                    <li className="menu__list">
+                        <a href="/books" className='menu__link'>
+                            Books
+                        </a>
+                    </li>
+                    <li className="menu__list">
+                        <a href="/cart" className='menu__link'>
+                            Cart
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
   )
